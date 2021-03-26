0623a2ac9620f8b60d4051125f507976f62946f7
Valeriy <vmahanov@gmail.com>
2021-03-27 00:31:50 +0300

    RepeatedRepairReport - fix

--- app/reports/repeated_repair_report.rb
+++ app/reports/repeated_repair_report.rb
@@ -5,7 +5,7 @@ class RepeatedRepairReport < BaseReport
     jobs = ServiceJob.select('item_id, device_type_id, serial_number, imei, count(*) qty')
                      .group('item_id, device_type_id, serial_number, imei').having('count(*) > 1')
     jobs = jobs.in_department(department) if department
-    jobs = jobs.where(created_at: start_date..end_date)
+    jobs = jobs.where(created_at: period)
     result[:jobs] = jobs
     result
   end
