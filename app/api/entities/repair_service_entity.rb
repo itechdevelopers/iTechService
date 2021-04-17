38149e51b569e748e5ec1f1b82943fb508c4f75a
Vladimir Kochergin <kvn047@gmail.com>
2021-04-17 21:27:06 +1000

    Update Repair services API

--- app/api/entities/repair_service_entity.rb
+++ app/api/entities/repair_service_entity.rb
@@ -1,7 +1,7 @@
 class Entities::RepairServiceEntity < Grape::Entity
   expose :id, :name, :client_info
-  expose :price do |repair_service, options|
-    repair_service.price(options[:store].department)
+  expose :prices do |repair_service, options|
+    Hash[options[:departments].map { |department| [department.short_name, repair_service.price(department)] }]
   end
   expose :status do |repair_service, options|
     status = repair_service.remnants_s(options[:store])
@@ -9,7 +9,12 @@ class Entities::RepairServiceEntity < Grape::Entity
   end
   expose :spare_parts do |repair_service, options|
     repair_service.spare_parts.map do |spare_part|
-      {id: spare_part.id, name: spare_part.name, quantity: spare_part.quantity_in_store(options[:store])}
+      quantity = Hash[
+        options[:departments].map do |department|
+          [department.short_name, spare_part.quantity_in_store(department.spare_parts_store)]
+        end
+      ]
+      {id: spare_part.id, name: spare_part.name, quantity: quantity}
     end
   end
 end
--- app/api/repair_api.rb
+++ app/api/repair_api.rb
@@ -9,23 +9,17 @@ class RepairApi < Grape::API
   end
   get 'repair_services' do
     authorize :read, RepairService
-    department = params[:department_id] ? Department.find(params[:department_id]) : current_department
-    store = department.spare_parts_store
+    departments = Department.real
 
-    if store.present?
-      if params[:group_id].present?
-        repair_group = RepairGroup.find(params[:group_id])
-        repair_groups = repair_group.children
-        repair_services = repair_group.repair_services
-        present :repair_services, repair_services, with: Entities::RepairServiceEntity, store: store
-      else
-        repair_groups = RepairGroup.roots
-        repair_services = RepairService.all
-      end
-      present :repair_services, repair_services, with: Entities::RepairServiceEntity, store: store
-      present :groups, repair_groups, with: Entities::RepairGroupEntity
+    if params[:group_id].present?
+      repair_group = RepairGroup.find(params[:group_id])
+      repair_groups = repair_group.children
+      repair_services = repair_group.repair_services.includes(:prices, :spare_parts, :store_items)
+      present :repair_services, repair_services, with: Entities::RepairServiceEntity, departments: departments
     else
-      error!({error: 'Spare parts store undefined'}, 404)
+      repair_groups = RepairGroup.roots
     end
+
+    present :groups, repair_groups, with: Entities::RepairGroupEntity
   end
 end
