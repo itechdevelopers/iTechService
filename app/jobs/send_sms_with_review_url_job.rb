c9d149d9e23eb6159d0c88d0338ac78ec1dc5ff6
Valeriy <vmahanov@gmail.com>
2021-05-30 13:14:44 +0300

    reviews - Добавлена ссылка на работу. СМС разбито на 2 сообщения

--- app/jobs/send_sms_with_review_url_job.rb
+++ app/jobs/send_sms_with_review_url_job.rb
@@ -6,8 +6,10 @@ class SendSmsWithReviewUrlJob  < ActiveJob::Base
     review = Review.find review_id
     service_job = review.service_job
     message = Setting.request_review_text(service_job.department)
-    message = "#{message} #{root_url}review/#{review.token}"
-    send_sms = SendSMS.(number: review.phone, message: message)
-    review.update(sent_at: DateTime.now) if send_sms.success?
+    review_url = "#{root_url}review/#{review.token}"
+    if SendSMS.(number: review.phone, message: review_url).success? &&
+      SendSMS.(number: review.phone, message: message).success?
+      review.update(sent_at: DateTime.now)
+    end
   end
 end
