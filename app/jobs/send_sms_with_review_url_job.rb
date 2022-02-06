b262c143f8fc49f0a2005c84d9540f8bbca6f066
Valeriy <vmahanov@gmail.com>
2022-02-06 17:51:04 +0300

    SendSmsWithReviewUrlJob - debug

--- app/jobs/send_sms_with_review_url_job.rb
+++ app/jobs/send_sms_with_review_url_job.rb
@@ -5,14 +5,16 @@ class SendSmsWithReviewUrlJob  < ApplicationJob
   def perform(review_id)
     review = Review.find review_id
     service_job = review.service_job
-    message = Setting.request_review_text(service_job.department)
-    review_url = "#{root_url}review/#{review.token}"
-    s1 = SendSMS.call(number: review.phone, message: message).success?
-    s2 = SendSMS.call(number: review.phone, message: review_url).success?
+    puts message = Setting.request_review_text(service_job.department)
+    puts review_url = "#{root_url}review/#{review.token}"
+    puts s1 = SendSMS.call(number: review.phone, message: message).success?
+    sleep(1)
+    puts s2 = SendSMS.call(number: review.phone, message: review_url).success?
     if s1 && s2
       review.update(sent_at: DateTime.now, status: :sent)
     else
       review.update(status: :error)
     end
+    puts review.status
   end
 end
