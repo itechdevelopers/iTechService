432a10e3119c8e81efe2c755806ba759577a3ab6
Vladimir <kvn047@gmail.com>
2022-06-20 08:55:48 +1000

    D

--- db/seeds_users.rb
+++
@@ -1,2 +0,0 @@
-User.create(username: 'admin', email: 'admin@itechstore.ru', role: 'admin', password: 'wrfswrfs', password_confirmation: 'wrfswrfs', card_number: '1b12abe8')
-User.create(username: 'vova', email: 'kvn@itechdevs.com', role: 'admin', password: 'qedaqeda', password_confirmation: 'qedaqeda', card_number: '7777')
