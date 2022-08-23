e76a7c5c5e32b0d413eb3d74ba4454c185a790ab
Vladimir <kvn047@gmail.com>
2022-08-23 19:52:31 +1000

    remove `puts`

--- app/jobs/send_sms_with_review_url_job.rb
+++ app/jobs/send_sms_with_review_url_job.rb
@@ -5,16 +5,16 @@ class SendSmsWithReviewUrlJob  < ApplicationJob
   def perform(review_id)
     review = Review.find review_id
     service_job = review.service_job
-    puts message = Setting.request_review_text(service_job.department)
-    puts review_url = "#{root_url}review/#{review.token}"
-    puts s1 = SendSMS.call(number: review.phone, message: message).success?
+    message = Setting.request_review_text(service_job.department)
+    review_url = "#{root_url}review/#{review.token}"
+    s1 = SendSMS.call(number: review.phone, message: message).success?
     sleep(1)
-    puts s2 = SendSMS.call(number: review.phone, message: review_url).success?
+    s2 = SendSMS.call(number: review.phone, message: review_url).success?
     if s1 && s2
       review.update(sent_at: DateTime.now, status: :sent)
     else
       review.update(status: :error)
     end
-    puts review.status
+    review.status
   end
 end
