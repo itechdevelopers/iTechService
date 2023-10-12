275d91c230950964fb85e0ac4e2152f397821a8c
Diana Schegoleva <dschegoleva@gmail.com>
2023-10-12 23:46:09 +0400

    Show wiki categories with access rights

--- app/models/wiki_page_category.rb
+++ app/models/wiki_page_category.rb
@@ -2,4 +2,11 @@ class WikiPageCategory < ApplicationRecord
   has_many :wiki_pages
 
   validates :title, presence: true
+  
+  scope :only_non_senior, -> {
+    joins(:wiki_pages)
+      .where(wiki_pages: { senior: false })
+      .group('wiki_page_categories.id')
+      .having('COUNT(wiki_pages.id) = ?', WikiPage.senior.count)
+  }
 end
