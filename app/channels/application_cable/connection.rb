81f40e7be23285cd776f6b5364a77fd30cd4dda1
Vladimir <kvn047@gmail.com>
2023-11-28 21:38:41 +1000

    Fix action cable auth

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -9,7 +9,7 @@ module ApplicationCable
     private
 
     def find_user
-      if user = env['warden'].user
+      if (user = env['warden']&.user)
         user
       else
         reject_unauthorized_connection
