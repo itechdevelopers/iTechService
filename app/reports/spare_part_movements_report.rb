d6336d1fbca55179b13b6240a1650bc01e7ccf14
Diana Schegoleva <dschegoleva@gmail.com>
2023-12-04 22:34:15 +0400

    Different parts have their own records

--- app/reports/spare_part_movements_report.rb
+++ app/reports/spare_part_movements_report.rb
@@ -31,13 +31,13 @@ class SparePartMovementsReport < BaseReport
     movements = []
     purchases = Purchase.where(date: period, store_id: all_department_store_ids)
                         .joins(:items)
-                        .where(items: { product_id: spare_part_id })
+                        .where(items: { product_id: sp.id })
                         .distinct
     purchases.each do |p|
       res = {}
       res[:date] = p.date
       res[:department] = p.store.present? ? p.store.name : "-"
-      res[:quantity] = p.batches.where(item: product_items).first.quantity
+      res[:quantity] = p.batches.where(item: product_items(sp)).first.quantity
       res[:doc_type] = "Приход"
       res[:doc_number] = "№#{p.id}"
       res[:comment] = p.comment.present? ? p.comment : "-"
@@ -49,13 +49,13 @@ class SparePartMovementsReport < BaseReport
                        .or(MovementAct.where(dst_store_id: all_department_store_ids))
                        .where(date: period)
                        .joins(:movement_items)
-                       .where(movement_items: { item: product_items })
+                       .where(movement_items: { item: product_items(sp) })
                        .distinct
     moves.each do |m|
       res = {}
       res[:date] = m.date
       res[:department] = "#{m.store.name} &#8680; #{m.dst_store.name}"
-      res[:quantity] = m.movement_items.where(item: product_items).first.quantity
+      res[:quantity] = m.movement_items.where(item: product_items(sp)).first.quantity
       res[:doc_type] = m.dst_store.kind == "defect_sp" ? "Брак" : "Перемещение"
       res[:doc_number] = "№#{m.id}"
       res[:comment] = m.comment.present? ? m.comment : "-"
@@ -65,13 +65,13 @@ class SparePartMovementsReport < BaseReport
 
     deductions = DeductionAct.where(date: period, store_id: all_department_store_ids)
                              .joins(:deduction_items)
-                             .where(deduction_items: { item: product_items })
+                             .where(deduction_items: { item: product_items(sp) })
                              .distinct
     deductions.each do |d|
       res = {}
       res[:date] = d.date
       res[:department] = "#{d.store.name}"
-      res[:quantity] = d.deduction_items.where(item: product_items).first.quantity
+      res[:quantity] = d.deduction_items.where(item: product_items(sp)).first.quantity
       res[:doc_type] = "Списание"
       res[:doc_number] = "№#{d.id}"
       res[:comment] = d.comment.present? ? d.comment : "-"
@@ -82,13 +82,13 @@ class SparePartMovementsReport < BaseReport
     service_jobs = ServiceJob.done
                              .where(done_at: period, department: department)
                              .joins(:repair_parts)
-                             .where(repair_parts: { item: product_items })
+                             .where(repair_parts: { item: product_items(sp) })
                              .distinct
     service_jobs.each do |s|
       res = {}
       res[:date] = s.done_at
       res[:department] = "#{s.department.name}"
-      res[:quantity] = s.repair_parts.where(item: product_items).first.quantity
+      res[:quantity] = s.repair_parts.where(item: product_items(sp)).first.quantity
       res[:doc_type] = "Реализация"
       res[:doc_number] = "№#{s.id}"
       res[:comment] = "-"
@@ -103,7 +103,7 @@ class SparePartMovementsReport < BaseReport
     department.map(&:stores).flatten.map { |st| st.id }
   end
 
-  def product_items
-    @items ||= Item.where(product_id: spare_part_id)
+  def product_items(sp)
+    @items ||= Item.where(product_id: sp.id)
   end
 end
