dc3fac6c45c1c92e156db76a6d992560ef67bd3c
Diana Schegoleva <dschegoleva@gmail.com>
2024-02-13 07:57:19 +0400

    Change photo uploading process

--- app/uploaders/service_job_photo_uploader.rb
+++ app/uploaders/service_job_photo_uploader.rb
@@ -1,15 +1,31 @@
 class ServiceJobPhotoUploader < ApplicationUploader
   permissions 0777
 
-  process resize_and_pad_white: [750, 1000]
+  process :resize_and_pad_white
 
   version :thumb do
-    process resize_and_pad_white: [30, 40]
+    process :resize_and_pad_white_small
   end
 
-  def resize_and_pad_white(x, y)
+  def resize_and_pad_white
     manipulate! do |img|
       img.combine_options do |c|
+        x = img[:width] * 0.7
+        y = img[:height] * 0.7
+        c.resize "#{x}x#{y}"
+        c.background "#ffffff"
+        c.gravity "Center"
+        c.extent "#{x}x#{y}"
+      end
+      img
+    end
+  end
+
+  def resize_and_pad_white_small
+    manipulate! do |img|
+      img.combine_options do |c|
+        x = 30
+        y = 40
         c.resize "#{x}x#{y}"
         c.background "#ffffff"
         c.gravity "Center"
