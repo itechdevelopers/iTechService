840e4e651ae5cb9f14c5f191092a89a3396a1506
Diana Schegoleva <dschegoleva@gmail.com>
2024-02-13 23:30:49 +0400

    Make photos look better

--- app/uploaders/service_job_photo_uploader.rb
+++ app/uploaders/service_job_photo_uploader.rb
@@ -10,8 +10,8 @@ class ServiceJobPhotoUploader < ApplicationUploader
   def resize_and_pad_white
     manipulate! do |img|
       img.combine_options do |c|
-        x = img[:width] * 0.7
-        y = img[:height] * 0.7
+        x = img[:width]
+        y = img[:height]
         c.resize "#{x}x#{y}"
         c.background "#ffffff"
         c.gravity "Center"
@@ -24,8 +24,8 @@ class ServiceJobPhotoUploader < ApplicationUploader
   def resize_and_pad_white_small
     manipulate! do |img|
       img.combine_options do |c|
-        x = 30
-        y = 40
+        x = 60
+        y = 80
         c.resize "#{x}x#{y}"
         c.background "#ffffff"
         c.gravity "Center"
