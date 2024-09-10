518f43fe0bf9e65f833c5e425bd380cb6718e582
Diana Shchegoleva <dschegoleva@gmail.com>
2024-09-10 12:25:36 +0400

    Fix repeated repair 2 report

--- app/reports/repeated_repair2_report.rb
+++ app/reports/repeated_repair2_report.rb
@@ -10,14 +10,14 @@ class RepeatedRepair2Report < BaseReport
           where item_id is not null
             and created_at >= '#{Date.parse(start_date).iso8601}'
             and created_at <= '#{Date.parse(end_date).iso8601}'
-            #{"'and location_id = ' #{department.id}" if department}
+            #{"and location_id = #{department.id}" if department}
           GROUP BY item_id HAVING count(*) > 1
       ) s2 on s1.item_id = s2.item_id
 
       where s1.item_id is not null and s2.c > 1
         and created_at >= '#{Date.parse(start_date).iso8601}'
         and created_at <= '#{Date.parse(end_date).iso8601}'
-        #{"'and location_id = ' #{department.id}" if department}
+        #{"and location_id = #{department.id}" if department}
       order by s2.c desc, s1.item_id desc, s1.created_at desc
     SQL
     jobs = ServiceJob.find_by_sql sql
