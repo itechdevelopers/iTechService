8dd231043c84d6eb6e04e3041cd00c2c7d3b94a1
Diana Shchegoleva <dschegoleva@gmail.com>
2024-09-11 23:18:06 +0400

    Add ability to change elqueue ticket depending on info

--- app/pdfs/waiting_client_pdf.rb
+++ app/pdfs/waiting_client_pdf.rb
@@ -6,7 +6,7 @@ class WaitingClientPdf < Prawn::Document
     @department = @waiting_client.electronic_queue.department
     @view = view
 
-    super page_size: [80.mm, 90.mm], page_layout: :portrait, margin: [10, 5, 10, 24]
+    super page_size: [80.mm, page_height], page_layout: :portrait, margin: [10, 5, 10, 24]
     font_families.update 'DroidSans' => {
       normal: "#{Rails.root}/app/assets/fonts/droidsans-webfont.ttf",
       bold: "#{Rails.root}/app/assets/fonts/droidsans-bold-webfont.ttf"
@@ -17,7 +17,7 @@ class WaitingClientPdf < Prawn::Document
   end
 
   def page_height_mm
-    90
+    page_height / 1.mm
   end
 
   private
@@ -33,7 +33,7 @@ class WaitingClientPdf < Prawn::Document
 
     move_down 50
     font_size 12 do
-      text @view.t("waiting_clients.pdf.your_queue_number"), align: :center, inlign_format: true, style: :bold
+      text @view.t("waiting_clients.pdf.your_queue_number"), align: :center, inline_format: true, style: :bold
     end
 
     move_down 5
@@ -47,14 +47,49 @@ class WaitingClientPdf < Prawn::Document
 
     move_cursor_to 50
     font_size 10 do
-      text @view.t("waiting_clients.pdf.discover_of_goods")
-      text @view.t("waiting_clients.pdf.site_url")
-      stroke_line [0, cursor], [80, cursor]
+      text waiting_client.electronic_queue.check_info
     end
   end
 
   def logo
     return unless department.logo_path
     image department.logo_path, width: 50, at: [0, cursor]
+  end
+
+  def page_height
+    line_height_medium = 13
+    line_height_small = 12
+    medium_symbol_lines = count_lines(waiting_client.queue_item.annotation, 27)
+    small_symbol_lines = count_lines(waiting_client.electronic_queue.check_info, 32)
+    line_height_medium * medium_symbol_lines + line_height_small * small_symbol_lines + 130
+  end
+
+  def count_lines(text, max_chars_per_line = 30)
+    return 0 if text.blank?
+
+    lines = 0
+    current_line_chars = 0
+
+    # Разбиваем текст на строки, сохраняя пустые строки
+    text.lines(chomp: true).each do |line|
+      # Обрабатываем каждую строку отдельно
+      line.split(/\s+/).each do |word|
+        word = word.strip
+
+        # Проверяем, поместится ли слово в текущую строку
+        if current_line_chars + word.length + (current_line_chars > 0 ? 1 : 0) > max_chars_per_line
+          lines += 1
+          current_line_chars = word.length
+        else
+          current_line_chars += word.length + (current_line_chars > 0 ? 1 : 0)
+        end
+      end
+
+      # Завершаем текущую строку
+      lines += 1
+      current_line_chars = 0
+    end
+
+    lines
   end
 end
