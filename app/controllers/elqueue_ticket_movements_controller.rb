9a04928c7fc1166151a3663f7337b2007827bb9e
Diana Shchegoleva <dschegoleva@gmail.com>
2024-11-20 15:22:12 +0400

    Fix bugs in user search and monitoring time

--- app/controllers/elqueue_ticket_movements_controller.rb
+++ app/controllers/elqueue_ticket_movements_controller.rb
@@ -10,7 +10,8 @@ class ElqueueTicketMovementsController < ApplicationController
                                               electronic_queue_id: elqueue_ticket_movements_params[:electronic_queue_id])
                                        .pluck(:waiting_client_id)
     tickets = WaitingClient.where(id: ids).order(ticket_called_at: :asc)
-    archived_tickets = WaitingClient.where(status: 'archived')
+    archived_tickets = WaitingClient.where(status: 'archived',
+                                           ticket_issued_at: start_datetime(date_str, time_str)..end_datetime)
     @tickets = tickets + archived_tickets
     @movements = ElqueueTicketMovement.where(waiting_client: @tickets).select(:type, :priority, :waiting_client_id)
 
@@ -27,7 +28,8 @@ class ElqueueTicketMovementsController < ApplicationController
                            .where(ticket_called_at: date_s.beginning_of_day..date_s.end_of_day,
                                   ticket_number: ticket_number,
                                   queue_items: { electronic_queue_id: electronic_queue_id })
-    archived_tickets = WaitingClient.where(status: :archived, ticket_number: ticket_number)
+    archived_tickets = WaitingClient.where(status: :archived, ticket_number: ticket_number,
+                                           ticket_issued_at: date_s.beginning_of_day..date_s.end_of_day)
     @tickets = tickets + archived_tickets
     @movements = ElqueueTicketMovement.where(waiting_client_id: @tickets).select(:type, :priority, :waiting_client_id)
     render 'filter_movements_by_called'
@@ -46,8 +48,12 @@ class ElqueueTicketMovementsController < ApplicationController
   def start_datetime(date_str, time_str)
     @start_datetime ||= begin
       date = Date.strptime(date_str, '%d.%m.%Y')
-      time = Time.new(date.year, date.month, date.day, time_str.to_i)
-      time.to_datetime
+      Time.zone.local(
+        date.year,
+        date.month,
+        date.day,
+        time_str.to_i
+      )
     end
   end
 
