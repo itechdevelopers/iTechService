0316a5c02e53741a3e51046ee2d75b87189b3369
Diana Shchegoleva <dschegoleva@gmail.com>
2024-11-20 15:35:18 +0400

    Change back user search implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -218,13 +218,7 @@ class User < ApplicationRecord
     end
 
     unless (q_name = params[:name]&.strip).blank?
-      # users = users.where 'users.username ILIKE :q or users.name ILIKE :q or users.surname ILIKE :q', q: "%#{q_name}%"
-      users = users.where(
-        "username ILIKE :q COLLATE \"ru_RU.UTF-8\" OR
-       name ILIKE :q COLLATE \"ru_RU.UTF-8\" OR
-       surname ILIKE :q COLLATE \"ru_RU.UTF-8\"",
-        q: "%#{q_name}%"
-      )
+      users = users.where 'users.username ILIKE :q or users.name ILIKE :q or users.surname ILIKE :q', q: "%#{q_name}%"
     end
     users
   end
