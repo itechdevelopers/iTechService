0a34766028afe47e8506e9471424a4dad8519f36
Diana Shchegoleva <dschegoleva@gmail.com>
2025-05-28 10:40:43 +0300

    fix: allow to add several questions to a checkList

--- app/models/check_list_item.rb
+++ app/models/check_list_item.rb
@@ -6,16 +6,6 @@ class CheckListItem < ApplicationRecord
   belongs_to :check_list
 
   validates :question, presence: true
-  validates :position, presence: true, uniqueness: { scope: :check_list_id }
 
-  before_validation :set_position, on: :create
-
-  private
-
-  def set_position
-    return if position.present?
-
-    max_position = check_list&.check_list_items&.maximum(:position) || 0
-    self.position = max_position + 1
-  end
+  acts_as_list scope: :check_list
 end
