5fb17b76a3e74fd63b90ebbcb51a6b3e6fab466f
Diana Shchegoleva <dschegoleva@gmail.com>
2025-06-02 05:53:17 +0300

    feat: add check_list_responses to tradeindevice

--- app/concepts/trade_in_device/cell/form.rb
+++ app/concepts/trade_in_device/cell/form.rb
@@ -41,5 +41,34 @@ module TradeInDevice::Cell
     def departments
       Department.select(:id, :name).selectable.map { |department| [department.name, department.id] }
     end
+  
+    def available_check_lists
+      @available_check_lists ||= CheckList.active.for_entity('TradeInDevice')
+    end
+
+    def check_list_responses
+      @check_list_resposnses ||= prepare_check_list_responses
+    end
+
+    def check_list_responses_hash
+      @check_list_responses_hash ||= check_list_responses.index_by(&:check_list_id)
+    end
+
+    def prepare_check_list_responses
+      available_check_lists.map do |check_list|
+        existing = model.check_list_responses.find { |r| r.check_list_id == check_list.id }
+        response = existing || model.check_list_responses.build(check_list: check_list)
+        response
+      end
+    end
+
+    def check_list_items_for(check_list)
+      check_list.check_list_items.ordered
+    end
+
+    def response_for_item(response, item)
+      return false unless response.persisted?
+      response.answer_for_item(item.id) || false
+    end
   end
 end
