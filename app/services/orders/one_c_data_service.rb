8d674dad2d001ea8c02cd7fae63c17f8ee13aa2e
Diana Shchegoleva <dschegoleva@gmail.com>
2025-09-09 01:33:40 +0300

    update order 1c integration

--- app/services/orders/one_c_data_service.rb
+++ app/services/orders/one_c_data_service.rb
@@ -17,17 +17,33 @@ module Orders
           article: order.article,
           price: order.approximate_price&.to_s,
           preorder: !order.source_store.present?,
-          source_store_id: order.source_store_id.to_s,
+          department_from_id: order.source_department&.code_one_c.to_s,
           source_store_name: order.source_store&.name,
           is_available_in_stock: order.source_store.present?,
           quantity: order.quantity,
-          comment: order.comment,
+          comment: build_comment_with_user_info,
           desired_date: order.desired_date&.strftime('%Y-%m-%d'),
           app_order_url: "https://ise.itech.pw/orders/#{order.id}",
           order_number: order.number,
           order_date: Time.current.strftime('%Y-%m-%d')
         }
       }
+    end
+  end
+
+  private
+
+  def build_comment_with_user_info
+    user_info = if order.user.present?
+      "Создал заказ сотрудник: #{order.user.short_name}, id сотрудника: #{order.user.id}"
+    else
+      "Создал заказ сотрудник: Не указан"
+    end
+    
+    if order.comment.present?
+      "#{order.comment}\n\n#{user_info}"
+    else
+      user_info
     end
   end
 end
