efe3e3df002860130c5a2a6f0acda22a87330ef2
Diana Shchegoleva <dschegoleva@gmail.com>
2026-02-04 11:27:42 +0300

    feat: add weekly hours calculation to schedule table

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -5,4 +5,9 @@ class Shift < ApplicationRecord
   validates :short_name, length: { maximum: 10 }, allow_blank: true
 
   default_scope { order(:position) }
+
+  def duration_hours
+    return 0 unless start_time && end_time
+    ((end_time - start_time) / 3600.0).round(1)
+  end
 end
