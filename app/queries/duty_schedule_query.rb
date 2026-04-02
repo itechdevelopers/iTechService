9f6c2c670c2b2da8ef2067fef074ac8d25f39d16
Diana Shchegoleva <dschegoleva@gmail.com>
2026-04-02 16:50:27 +0300

    fix: use correct column names for employee sorting in duty schedule

--- app/queries/duty_schedule_query.rb
+++ app/queries/duty_schedule_query.rb
@@ -32,7 +32,7 @@ class DutyScheduleQuery
           .where(id: user_ids)
           .joins(:location)
           .where(locations: { schedule: true })
-          .order('locations.name ASC, users.last_name ASC, users.first_name ASC')
+          .order('locations.name ASC, users.surname ASC, users.name ASC')
     end
   end
 
